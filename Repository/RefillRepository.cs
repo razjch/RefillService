86deafff12ef45939565c8d8760c32a2891921bd
razjch <64721517+razjch@users.noreply.github.com>
2021-09-25 16:25:06 +0545

    Update RefillRepository.cs

--- Repository/RefillRepository.cs
+++ Repository/RefillRepository.cs
@@ -22,7 +22,7 @@ namespace RefillService.Repository
         public RefillOrder RequestAdhocRefill(SubscriptionDTO subscriptionDTO)
         {
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://20.84.192.31");
+            client.BaseAddress = new Uri("https://subscriptionservice1.azurewebsites.net");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", subscriptionDTO.Token);
 
             //StringContent content = new StringContent(JsonConvert.SerializeObject(""), Encoding.UTF8, "application/json");
